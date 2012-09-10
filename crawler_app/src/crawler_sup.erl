3f2b8f1c8978d823fb8688554a0820ae1fff51f0
jason <mentel.szymon@gmail.com>
2012-09-10 09:48:38 +0200

    Rebar configured.

---
+++ crawler_app/src/crawler_sup.erl
@@ -0,0 +1,31 @@
+
+-module(crawler_sup).
+
+-behaviour(supervisor).
+
+%% API
+-export([start_link/0]).
+
+%% Supervisor callbacks
+-export([init/1]).
+
+%% Helper macro for declaring children of supervisor
+-define(CHILD(I, Type), {I, {I, start_link, []}, permanent, 5000, Type, [I]}).
+
+%% ===================================================================
+%% API functions
+%% ===================================================================
+
+start_link() ->
+    supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+%% ===================================================================
+%% Supervisor callbacks
+%% ===================================================================
+
+init([]) ->
+    MineSrvWorker = ?CHILD(mine_srv , worker), 
+    Children = [ MineSrvWorker ], 
+    RestartStrategy = { one_for_one , 4, 9600},
+    {ok, { RestartStrategy, Children } }. 
+
