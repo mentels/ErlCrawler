52f483f3b201063d6f9515013bd6992ae1f5a1c6
Szymon Mentel <szymon.mentel@backrub2.iisg.agh.edu.pl>
2013-01-24 13:45:18 +0100

    git ignore fixed, name fixed, worksers set to 100

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-VM_NAME="erlcrawler@`hostname`"
+VM_NAME="erlcrawler@`hostname -I | awk -F' ' '{print$1}'`"
 DEPS="deps/bson/ebin/  deps/mongodb/ebin/  deps/lager/ebin/  apps/crawler_persistence_app/ebin  deps/ibrowse/ebin/  deps/mochiweb/ebin/  apps/crawler_app/ebin/"
 erl -pa ${DEPS} -name ${VM_NAME}
