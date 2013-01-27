ca31a77794b4cc9e7415857aff5d12a34fa2a401
Maciek M. <maciekdg@gmail.com>
2013-01-27 11:27:05 +0100

    poprawa formatu logowania stats_servera

--- apps/crawler_app/src/stats_server.erl
+++ apps/crawler_app/src/stats_server.erl
@@ -26,8 +26,9 @@ handle_call(write_stats, From, {PageTotal,BinaryTotal,WordsTotal,File}) ->
 	{ok,Words_coll} = data_api:get_count(words_coll),
 	{ok,Index_coll} = data_api:get_count(index_coll),
 	Memory = erlang:memory(total),
+	{Mega_sec,Sec,Milisec} = erlang:now(),
 	{{Year,Month,Day},{Hour,Minute,Seconds}}=calendar:now_to_local_time(erlang:now()),
-	io:format(File,"~w-~w-~w ~w:~w:~w Page_total: ~w Binary_total ~w Words_processed_total ~w Indexes ~w Words_coll ~w Index_coll ~w Memory ~w\n",[Year,Month,Day,Hour,Minute,Seconds,PageTotal,BinaryTotal,WordsTotal, Indexes, Words_coll,Index_coll,Memory]),
+	io:format(File,"~w-~w-~w ~w:~w:~w ~w~w~w Page_total: ~w Binary_total ~w Words_processed_total ~w Indexes ~w Words_coll ~w Index_coll ~w Memory ~w\n",[Year,Month,Day,Hour,Minute,Seconds,Mega_sec,Sec,Milisec,PageTotal,BinaryTotal,WordsTotal, Indexes, Words_coll,Index_coll,Memory]),
 	{reply,ok,{PageTotal,BinaryTotal,WordsTotal,File}};
 
 handle_call({add_page_stats,Binary_len,Words_len},From,{PageTotal,BinaryTotal,WordsTotal,File}) ->
