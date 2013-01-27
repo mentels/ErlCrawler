98bf21ea24d925ad0dbd9d41aba617d35d3ce2d7
Maciek M. <maciekdg@gmail.com>
2013-01-27 15:31:57 +0100

    zmiana nazwy nodów, na potrzeby testu

--- GUI/test/test_node.sh
+++ GUI/test/test_node.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 erlc data_api.erl
-NODE_NAME="`hostname`@`hostname -I | awk -F' ' '{print$1}'`"
+NODE_NAME="T`hostname`@`hostname -I | awk -F' ' '{print$1}'`"
 erl -pa . -setcookie NUJPVNQEPEZJBTISDVFY -name ${NODE_NAME}
