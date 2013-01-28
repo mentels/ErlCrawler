eb5a8c8c521b68982ac7e1c55e4458aac3c294d1
Maciek M. <maciekdg@gmail.com>
2013-01-28 16:33:52 +0100

    poprawki GUI : dołożenie konfigurcji

--- GUI/crawler/src/controller/crawler_config_controller.erl
+++ GUI/crawler/src/controller/crawler_config_controller.erl
@@ -6,4 +6,14 @@ stats('GET',[]) ->
 	{ok, [{stats,lists:map(fun(X) -> {X,utils:node_stats(X)} end , Nodes)}]}.
  
 configs('GET',[]) ->
- 	ok.
+	{ok,Nodes} = application:get_env(crawler,nodes),
+	{ok, [{confs,lists:map(fun(X) -> {X,utils:get_down_man_conf(X)} end , Nodes)}]}.
+
+save('POST',[])->
+	Node = Req:post_param("node"),
+	Max_Active_Workers = Req:post_param("max_active_workers"),
+	Max_redir = Req:post_param("max_redir"),
+	Down_timeout =Req:post_param("down_timeout"),
+	Conn_timeout = Req:post_param("conn_timeout"),
+	utils:set_down_man_conf(Node,{Max_Active_Workers,Max_redir,Down_timeout,Conn_timeout}),
+	{redirect, "/config/configs"}.
--- GUI/crawler/utils/src/utils.erl
+++ GUI/crawler/utils/src/utils.erl
@@ -7,11 +7,63 @@
 %% ====================================================================
 %% API functions
 %% ====================================================================
--export([query_nodes/4,query_nodes_4_word/3,node_stats/1]).
- 
+-export([query_nodes/4,query_nodes_4_word/3,node_stats/1,get_down_man_conf/1,set_down_man_conf/2]).
+
+
+set_down_man_conf(Node,{Max_Active_Workers,Max_redir,Down_timeout,Conn_timeout})->
+	rpc:call(list_to_atom(Node),download_manager,set_max_active_workers,[list_to_integer(Max_Active_Workers)],1000),
+	rpc:call(list_to_atom(Node),download_manager,set_max_redit,[list_to_integer(Max_redir)],1000),
+	rpc:call(list_to_atom(Node),download_manager,set_down_timeout,[list_to_integer(Down_timeout)],1000),
+	rpc:call(list_to_atom(Node),download_manager,set_conn_timeout,[list_to_integer(Conn_timeout)],1000),
+	rpc:call(list_to_atom(Node),download_manager,start_processing,[],1000),
+	ok.
+
+get_down_man_conf(Node)->
+
+	case rpc:call(Node,download_manager,get_max_active_workers,[],1000) of
+		{badrpc,_} ->
+			Res = [{max_active_workers,[]}];
+		{Answer} ->
+			Res =  	[{max_active_workers,Answer}];
+		_ ->
+			Res = [{max_active_workers,[]}]
+	end,
+	case rpc:call(Node,download_manager,get_current_active_workers,[],1000) of
+		{badrpc,_} ->
+			Res1 = Res ++ [{current_active_workers,[]}];
+		{Answer1} ->
+			Res1 = Res ++ [{current_active_workers,Answer1}];
+		_ ->
+			Res1 = Res ++ [{current_active_workers,[]}]
+	end,
+	case rpc:call(Node,download_manager,get_max_redir,[],1000) of
+		{badrpc,_} ->
+			Res2 = Res1 ++ [{max_redir,[]}];
+		{Answer2} ->
+			Res2 = Res1 ++ [{max_redir,Answer2}];
+		_ ->
+			Res2 = Res1 ++ [{max_redir,[]}]
+	end,
+	case rpc:call(Node,download_manager,get_down_timeout,[],1000) of
+		{badrpc,_} ->
+			Res3 = Res2 ++ [{down_timeout,[]}];
+		{Answer3} ->
+			Res3 = Res2 ++ [{down_timeout,Answer3}];
+		_ ->
+			Res3 = Res2 ++ [{down_timeout,[]}]
+	end,
+	case rpc:call(Node,download_manager,get_conn_timeout,[],1000) of
+		{badrpc,_} ->
+			Res4 = Res3 ++ [{conn_timeout,[]}];
+		{Answer4} ->
+			Res4 = Res3 ++ [{conn_timeout,Answer4}];
+		_ ->
+			Res4 = Res3 ++ [{conn_timeout,[]}]
+	end,
+	Res4.
 
 node_stats(Node) ->
-	case rpc:call(Node,erlang,memory,[total]) of
+	case rpc:call(Node,erlang,memory,[total],1000) of
 		{badrpc,_} ->
 			Res = [{memory_total,[]}];
 		Answer ->
@@ -19,7 +71,7 @@ node_stats(Node) ->
 		_ ->
 			Res =[{memory_total,[]}]
 	end,
-	case rpc:call(Node,erlang,statistics,[wall_clock]) of
+	case rpc:call(Node,erlang,statistics,[wall_clock],1000) of
 		{badrpc,_} ->
 			Res1 = Res ++ [{uptime,[]}];
 		{Total,_Since_Last_Call} ->
@@ -27,15 +79,15 @@ node_stats(Node) ->
 		_ ->
 			Res1 = Res ++ [{uptime,[]}]
 	end,
-	case rpc:call(Node,erlang,statistics,[io]) of
+	case rpc:call(Node,stats_server,get_stats,[],1000) of
 		{badrpc,_} ->
 			Res2 = Res1 ++ [{input_data,[]}];
-		{{input,Input},{output,_Output}} ->
-			Res2 = Res1 ++ [{input_data,[{Input div (1024*1024),"MB"},{(Input rem (1024*1024)) div 1024,"kB"}]}];
+		{_,BinaryTotal,_,_} ->
+			Res2 = Res1 ++ [{input_data,[{BinaryTotal div (1024*1024),"MB"},{(BinaryTotal rem (1024*1024)) div 1024,"kB"}]}];
 		_ ->
 			Res2 = Res1 ++ [{input_data,[]}]
 	end,
-	case rpc:call(Node,data_api,get_count,[indexes]) of
+	case rpc:call(Node,data_api,get_count,[indexes],1000) of
 		{badrpc,_} ->
 			Res3 = Res2 ++ [{indexes,[]}];
 		{ok,Value} ->
@@ -43,7 +95,7 @@ node_stats(Node) ->
 		_ ->
 			Res3 = Res2 ++ [{indexes,[]}]
 	end,
-	case rpc:call(Node,data_api,get_count,[words_coll]) of
+	case rpc:call(Node,data_api,get_count,[words_coll],1000) of
 		{badrpc,_} ->
 			Res4 = Res3 ++ [{words_coll,[]}];
 		{ok,Value2} ->
@@ -51,7 +103,7 @@ node_stats(Node) ->
 		_ ->
 			Res4 = Res3 ++ [{words_coll,[]}]
 	end,
-	case rpc:call(Node,data_api,get_count,[index_coll]) of
+	case rpc:call(Node,data_api,get_count,[index_coll],1000) of
 		{badrpc,_} ->
 			Res5 = Res4 ++ [{index_coll,[]}];
 		{ok,Value3} ->
@@ -59,13 +111,19 @@ node_stats(Node) ->
 		_ ->
 			Res5 = Res4 ++ [{index_coll,[]}]
 	end,
-	case rpc:call(Node,link_server,processed_links,[]) of
+	case rpc:call(Node,stats_server,get_stats,[],1000) of
 		{badrpc,_} ->
 			Res6 = Res5 ++ [{processed_links,[]}];
-		Value4 ->
-			Res6 = Res5 ++ [{processed_links,Value4}]
+		{Page_total,_,_,_} ->
+			Res6 = Res5 ++ [{processed_links,Page_total}]
 	end,
-	Res6.
+	case rpc:call(Node,stats_server,get_stats,[],1000) of
+		{badrpc,_} ->
+			Res7 = Res6 ++ [{words_total,[]}];
+		{_,_,Words_total,_} ->
+			Res7 = Res6 ++ [{words_total,Words_total}]
+	end,
+	Res7.
 query_nodes(Nodes,data_api,get_index,Args) ->
 	query_nodes_4_word(Nodes, Args, []);
 
